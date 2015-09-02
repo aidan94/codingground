da574c7e1ee0ef41ba777db6e9e7dc37a77fec64
aidan94 <aidanyeoh94@gmail.com>
2015-09-02 01:02:39 -0700

    Created by Coding Ground Client

---
+++ New Project/Testing.java
@@ -0,0 +1,10 @@
+public class Testing {
+    public static void main(String[]args)
+    {
+        Point a = new Point();
+        Point b = new Point (4,-3);
+        
+        System.out.println ("Point a is " + a.toString());
+        System.out.println("Point b is " + b.toString());
+    }
+}
