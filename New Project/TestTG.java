55792d78328604626086ce3b43b565ef61563614
aidan94 <aidanyeoh94@gmail.com>
2015-09-02 01:02:39 -0700

    Created by Coding Ground Client

---
+++ New Project/TestTG.java
@@ -0,0 +1,28 @@
+public class TestTG {
+    //Create new Tutegroup
+    static TuteGroup tg = new TuteGroup("BIT203");
+
+public static void main (String[] args)
+    {
+  
+    //allow students to sign up their preferred tutorial group 
+    // with success and failed examples
+    if (tg.addStudent("Ng Shu Min", TuteGroup.ONE));
+        System.out.println("Student added");
+    
+    
+	//print on the console screen the number of students who have signed up their tutorial group; 
+    //print on the console screen the number of students who have signed up for a particular tutorial group;
+    //print on the console screen the names of all students who have already signed up their tutorial group;
+	//print on the console screen the names of student who have signed up for a particular tutorial group;
+	//search for a specific student given his/her name, 
+	//and display his/her tutorial group if success, 
+	//otherwise display an error message.
+   
+    
+       // Scanner sc = new Scanner(System.in);
+        //System.out.println("Tutorial grouping for BIT203");
+        //static TuteGroup tg = new TuteGroup("BIT203");
+      
+    }
+}
